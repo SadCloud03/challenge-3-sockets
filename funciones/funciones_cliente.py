694431dad19aee0377f15f441754e3ef30bc0e1a
SadCloud03 <167452037+SadCloud03@users.noreply.github.com>
2026-01-21 20:07:49 -0300

    Add files via upload
    funciones que permiten que Cliente.py y Servidor.py puedan funcionar correctamente

---
+++ funciones/funciones_cliente.py
@@ -0,0 +1,34 @@
+import threading
+import socket
+import time
+
+lock = threading.Lock()
+
+
+
+def conectar(PORT, HOST, num_reintentos = 3, tiempo_sleep = 1):
+    for intento in range(num_reintentos):
+        time.sleep(tiempo_sleep)
+        try:
+            socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket_cliente.connect((HOST, PORT))
+
+            return socket_cliente
+        except Exception as e:
+            print(f"[error] : [{e}] ; [reintento] : [{intento}]")
+
+
+
+def escuchar_mensajes(socket_cliente):
+    while True:
+        try:
+            mensaje = socket_cliente.recv(1024).decode()
+
+            if not mensaje:
+                break
+
+            print("\n" + mensaje)
+        except:
+            print("[error] : [No conexion to server]")
+            break
+
